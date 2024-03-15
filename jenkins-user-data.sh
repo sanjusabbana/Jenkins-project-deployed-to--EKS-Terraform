555eeac614b369c8d59cf572f13e78078bfaf982
sanjusabbana <sabbanasudhakar@gmail.com>
2024-03-15 23:45:49 +0530

    jenkins done

--- jenkins-user-data.sh
+++ jenkins-user-data.sh
@@ -26,3 +26,5 @@ sudo yum -y install terraform
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# everything is done
